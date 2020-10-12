b9959deea250572756b92ae8aafd36818c28bc7e
tetsu.kinoshita <rubbish-creator@docomo.ne.jp>
2020-10-12 04:59:19 +0000

    clone1

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,10 @@
 class TasksController < ApplicationController
+before_action :require_user_logged_in
+
     def index
-        @tasks = Task.all
+
+        @tasks = current_user.tasks.order(id: :desc).page(params[:page])
+
     end
 
     def show
@@ -12,12 +16,14 @@ class TasksController < ApplicationController
     end
 
     def create
-        @task = Task.new(task_params)
-    
+        
+        @task = current_user.tasks.build(task_params)
+       
         if @task.save
             flash[:success] = 'タスク・ステイタス が正常に投稿されました'
-            redirect_to @task
+            redirect_to root_url
         else
+            @tasks = current_user.tasks.order(id: :desc).page(params[:page])
             flash.now[:danger] = 'タスク・ステイタス が投稿されませんでした'
             render :new
         end
